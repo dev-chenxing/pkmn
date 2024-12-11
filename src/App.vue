68d4219b91772802e565cbf5e86454a4706d1f99
陈刑 <chenxing.io@outlook.com>
2024-12-11 12:50:00 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const query = ref('')
+const inputValue = ref('')
+function onsubmit() {
+  if (inputValue.value) {
+    query.value = inputValue.value
+    inputValue.value = ''
+  }
+}
+</script>
+
+<template>
+  <header class="flex">
+    <form class="w-full" @submit.prevent="onsubmit">
+      <span>Pokedex> </span>
+      <input
+        type="search"
+        autocomplete="off"
+        placeholder="Search Pokedex"
+        class="bg-black"
+        v-model="inputValue"
+      />
+    </form>
+  </header>
+  <section>
+    <p>{{ query }}</p>
+  </section>
+</template>
+
+<style scoped></style>
